ec1eeab58e17d03f404effa1cb4fc8e87c3b68bf
TechAnhDev <techanhdev@gmail.com>
2024-05-15 08:47:02 +0700

    push project

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import GlobalStyles from '~/components/GlobalStyles/GlobalStyles.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <GlobalStyles>
+    <App />
+  </GlobalStyles>
+  // </React.StrictMode>,
+)
